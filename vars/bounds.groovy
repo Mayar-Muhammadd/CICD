8e0f7e1ceb040aaec038d942fa49e14482b7ac17
Mayar-Muhammadd <mayarrashwan67@gmail.com>
2025-08-26 07:24:45 +0300

    Add bounds.groovy shared library function

--- vars/bounds.groovy
+++ vars/bounds.groovy
@@ -1,1 +1,7 @@
+def call(int lower, int upper) {
+    if (lower > upper) {
+        error "Lower bound cannot be greater than upper bound!"
+    }
+    return (lower..upper).toList()
+}
 def call(int x) { return x * 2 }
